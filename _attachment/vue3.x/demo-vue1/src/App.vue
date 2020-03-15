1bdab29ce0d302e18ff767a03e119b210f08d8b8
村口蹲一郎 <ismb@qq.com>
2020-03-15 12:14:55 +0800

    add vue3.x demo1

---
+++ _attachment/vue3.x/demo-vue1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Hello Vue 3!</h1>
+    <button @click="inc">Clicked {{ count }} times.</button>
+</template>
+
+<script>
+    import { ref } from 'vue'
+
+    export default {
+        setup() {
+            const count = ref(0)
+            const inc = () => {
+                count.value++
+            }
+
+            return {
+                count,
+                inc
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>

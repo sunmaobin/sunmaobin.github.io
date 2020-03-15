c9729204f97684590ff5e414a473a3c942e15c81
村口蹲一郎 <ismb@qq.com>
2020-03-15 12:43:38 +0800

    add vue3.x demo 2 & 3

---
+++ _attachment/demo-vue2/src/views/Page1.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>Page1</h2>
+</template>
+
+<script>
+    import store from '../store';
+    export default {
+        name: 'page2',
+        setup() {
+            // 更新 visit 全局变量
+            store.updateCnt();
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ _attachment/demo-vue3/src/views/Page2.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>Ts Page2</h2>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import store from '../store';
+    export default defineComponent({
+        name: 'page1',
+        setup() {
+            // 更新 visit 全局变量
+            store.updateCnt();
+        }
+    })
+</script>
+
+<style scoped>
+</style>
